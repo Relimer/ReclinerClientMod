7cecbe8426a6060d2bb15a9fcf4fa111643a7ea8
Relimer <digglejj@gmail.com>
2022-12-09 22:57:04 +0000

    initial commit

---
+++ src/main/java/net/relimer/reclinerclientmod/ReclinerClientModClient.java
@@ -0,0 +1,10 @@
+package net.relimer.reclinerclientmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReclinerClientModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
